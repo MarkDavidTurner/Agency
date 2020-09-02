cdb0ed46f2b055412df8bebc69752d49a48421bd
Mark Turner <mark.turner@rapiddeploy.com>
2020-09-02 15:18:51 +0200

    Package update

---
+++ application/agency/core/App.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _notesRd = _interopRequireDefault(require("notes-rd"));
+
+var App = function App(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("h1", null, title), /*#__PURE__*/_react["default"].createElement(_notesRd["default"], null, "Package Button"));
+};
+
+var _default = App;
+exports["default"] = _default;
---
+++ packages/Notes/core/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _styledComponents = _interopRequireDefault(require("styled-components"));
+
+var StyledWrapper = _styledComponents["default"].button.withConfig({
+  displayName: "src__StyledWrapper",
+  componentId: "rwit6s-0"
+})(["background:#1ea7fd;color:#fff;border-radius:4px;cursor:pointer;font-size:1rem;font-weight:300;padding:12px 36px;border:none;"]);
+
+var Notes = function Notes(_ref) {
+  var children = _ref.children;
+  return /*#__PURE__*/_react["default"].createElement(StyledWrapper, null, /*#__PURE__*/_react["default"].createElement("h1", null, "Notes Component 01"), /*#__PURE__*/_react["default"].createElement("p", null, children));
+};
+
+var _default = Button;
+exports["default"] = _default;
